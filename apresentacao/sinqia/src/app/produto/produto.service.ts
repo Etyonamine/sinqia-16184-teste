d96eb57ea3e6379221b97f0e77c7724a46a04908
Etyonamine <60787694+Etyonamine@users.noreply.github.com>
2021-05-17 15:19:28 -0300

    Inclusao de descricao do home da versao do material angular

--- apresentacao/sinqia/src/app/produto/produto.service.ts
+++ apresentacao/sinqia/src/app/produto/produto.service.ts
@@ -1,4 +1,4 @@
-import { environment } from './../../environments/environment';
+import { environment } from './../../environments/environment.prod';
 import { BaseService } from './../shared/service/base.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -12,5 +12,5 @@ export class ProdutoService extends BaseService<Produto> {
   constructor(protected http : HttpClient) {
     super(http,`${environment.API}Produto`);
   }
-  
+
 }
